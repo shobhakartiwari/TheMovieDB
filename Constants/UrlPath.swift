45ad27443d4c75c1d5c00136b35fd7f5a2ac75d9
DaleAbdullahi <abdull53@msu.edu>
2020-07-05 18:25:23 -0500

    Created the constants for the general API url path  and thumbnail/poster path within  a single class i will use to call the returned paths . I usually dont like creating contents in the MainViewController . I could have used any other design as well . Just feel like calling directly encapsulated paths make sense .

---
+++ Constants/UrlPath.swift
@@ -0,0 +1,26 @@
+//
+//  UrlPath.swift
+//  TheMovieDB
+//
+//  Created by Duale on 7/5/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UrlPath {
+   static let path = UrlPath()
+   private init () {}
+   var api_url_path = "https://api.themoviedb.org/3/discover/movie?api_key="
+   var image_url_path = "https://image.tmdb.org/t/p/w500"
+   var API_KEY = "c91ed3a7a344459eccad9687acf0d07e"
+   func sharePathUrl()->String {
+       return api_url_path + API_KEY
+   }
+   func sharePathImg()->String {
+       return image_url_path
+   }
+
+}
+
