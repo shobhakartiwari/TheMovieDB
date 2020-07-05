ac94440bd373f79c51d42b22443775a019d2b729
DaleAbdullahi <abdull53@msu.edu>
2020-07-05 17:29:09 -0500

    Created the Networking functionalities to grab data for the API . Used escaping closure so that the closure outlives its function definition in order to grab an array of movies .  Implemented custom MovieCell also . Used a TableView instead of Custom Cell . It made sense to me that both can work on this specific task.

---
+++ View/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCell.swift
+//  TheMovieDB
+//
+//  Created by Duale on 7/5/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+    }
+
+}
