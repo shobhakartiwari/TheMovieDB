e8c6e4045a85c48b2ed6f6f11479ac67a26c41ec
Duale Abdullahi <abdull53@msu.edu>
2020-07-05 17:08:20 -0500

    Initial Commit

---
+++ TheMovieDB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovieDB
+//
+//  Created by Duale on 7/5/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
