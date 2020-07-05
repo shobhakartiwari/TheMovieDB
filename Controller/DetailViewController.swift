0d4ffc06547d3331af816350fe5481badd62245b
DaleAbdullahi <abdull53@msu.edu>
2020-07-05 17:36:11 -0500

    Starting the ViewControllers implementations . Initialized the MainViewController ( The Main View ) and the DetailViewController to navigate when a cell is clicked thus showing the details of a movie . I am using UINavigationController to manages child view controllers in the navigation interface

---
+++ Controller/DetailViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DetailViewController.swift
+//  TheMovieDB
+//
+//  Created by Duale on 7/5/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
