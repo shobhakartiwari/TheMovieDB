199084379c377c7957f0d53df579e0708c783a2d
DaleAbdullahi <abdull53@msu.edu>
2020-07-06 00:38:00 -0500

    Include a simple ReadMe as swift file for my reasons to use what i used and some other details

---
+++ ReadMe/ReadMe.swift
@@ -0,0 +1,36 @@
+//
+//  ReadMe.swift
+//  TheMovieDB
+//
+//  Created by Duale on 7/6/20.
+//  Copyright © 2020 Duale. All rights reserved.
+//
+
+import Foundation
+/*
+    THIS IS JUST FILE FOR REFRENCE:
+    
+     THIRD PARTY LIBRARIES:
+ 
+ 
+
+       I did not use any third part libary . I could have used Alamofire for networking and sdWebImage for image loading.
+       Instead i used URLSession for networking and loaded images using inbuilt swift functionalitis
+       I try to always mininise the use of third party libaries as much as i can since relying on third parties can some time bring problems
+        
+ 
+        My API link has no video or the movie link homepage so did not do that . I used every variable found in the API
+ 
+ 
+        Please run with physical iphone 11 or Iphone 11 simulator . I did layout with iphone 11
+ 
+ 
+        I used MVC . This is a simple app so both design work without major problems . I always prefer using MVVM instead of MVC all the time because of the ViewModel relieving a lot of stress from the ViewControllers and also easy testing of ViewModel functionalities compared to functionailties in the ViewControllers
+ 
+
+ 
+        I used extenstions almost everywhere to separate code logic with Marks.
+ 
+        
+    
+ */
