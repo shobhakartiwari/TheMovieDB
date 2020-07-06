1e04cb9623c91ae46ec70127a5eda6456472b367
DaleAbdullahi <abdull53@msu.edu>
2020-07-05 19:54:35 -0500

    Created Searching functionality where a user searches the Movies depending on Title . I could have use release year or any other variable as a searching mode but title makes the most sense . Added screenshots as well

--- Controller/MainViewController.swift
+++ Controller/MainViewController.swift
@@ -11,8 +11,10 @@ import UIKit
 class MainViewController: UIViewController {
    
     @IBOutlet weak var moviesTableView: UITableView!
+    let searchcontroller = UISearchController(searchResultsController: nil);
 //    let dummyArray : [UIColor] = [.red , .blue , .black , .gray]  // dummy array to test functionality . will remove later
 //    let arrayStrings : [String] = ["Red" , "Blue" , "Black" , "Gray"]
+    
     var movies = [Movie]()
     var networkHandler = Networking()
     var filteredMovs = [Movie]()
@@ -27,6 +29,9 @@ class MainViewController: UIViewController {
     }
     var releaseYHelper : String {
         return  "Release Year: "
+    }
+    var searchPlaceHolder : String {
+       return "Enter a movie title to search"
     }
     
     override func viewDidLoad() {
@@ -51,17 +56,21 @@ class MainViewController: UIViewController {
 
 extension MainViewController : UITableViewDataSource  {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        movies.count
+        // check if movie is filtered or not and getting the count we need depending on that
+        let count = (isfiltered) ? filteredMovs.count :  movies.count
+        return count
     }
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell" , for: indexPath) as! MovieCell
         guard let url = URL(string: UrlPath.path.sharePathImg() + movies[indexPath.row].poster_path) else {return UITableViewCell()}
         let data = try? Data(contentsOf: url)
         guard let datac = data else  {return UITableViewCell()}
+        // moviees depending on search
+        let moviess : [Movie] = (isfiltered) ? filteredMovs : movies
         cell.movieimage.beautify(image: UIImage(data: datac)!)
-        cell.title.beautify(str: titleHelper + movies[indexPath.row].title )
-        cell.popularityScore.beautify(str: populaHelper + String(movies[indexPath.row].popularity))
-        cell.releaseYear.beautify(str: releaseYHelper + GetReleaseYear(date: movies[indexPath.row].release_date))
+        cell.title.beautify(str: titleHelper + moviess[indexPath.row].title )
+        cell.popularityScore.beautify(str: populaHelper + String(moviess[indexPath.row].popularity))
+        cell.releaseYear.beautify(str: releaseYHelper + GetReleaseYear(date: moviess[indexPath.row].release_date))
         cell.containerView.Decorate()
         cell.Beautify()
         return cell
@@ -74,8 +83,17 @@ extension MainViewController : UITableViewDelegate   {
         tableView.deselectRow(at: indexPath, animated: true)
         let storyB = UIStoryboard(name: "Main", bundle: nil)
         guard let detVc = storyB.instantiateViewController(identifier: "DetailViewController") as? DetailViewController else {return}
-        detVc.navigationItem.title = movies[indexPath.row].title
+        let moviess : [Movie] = (isfiltered) ? filteredMovs : movies
+        detVc.navigationItem.title = moviess[indexPath.row].title
         navigationController?.pushViewController(detVc, animated: true)
+    }
+}
+
+//MARK:- SearchController delegate for updating
+extension MainViewController : UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+       filteredMovies(for: searchBar.text!)
     }
 }
 
@@ -86,6 +104,11 @@ extension MainViewController  {
             moviesTableView.dataSource = self
             moviesTableView.Beautify()
             navigationItem.title = navtTile
+            searchcontroller.searchResultsUpdater = self
+            navigationItem.searchController = searchcontroller
+            searchcontroller.searchBar.placeholder = searchPlaceHolder
+            searchcontroller.obscuresBackgroundDuringPresentation = false
+            definesPresentationContext = true
     }
     // grap the year of release. I found no need for date formater since the release year is in string and with "-" separator 
     func GetReleaseYear (date : String ) -> String {
@@ -93,12 +116,43 @@ extension MainViewController  {
            return y_m_d[0]
    }
     
-    // Configurating the tableview 
+    // Configurating the tableview
     func ConfigureCellView() {
         moviesTableView.rowHeight = UITableView.automaticDimension
         moviesTableView.estimatedRowHeight = 200
     }
   
       
+}
+
+//MARK:-  Filtering movies depending on users search
+extension MainViewController {
+       var isfiltered : Bool {
+           return searchcontroller.isActive  && !isfilteredEmpty
+       }
+       var isfilteredEmpty : Bool {
+           return searchcontroller.searchBar.text?.isEmpty ?? true
+       }
+}
+
+//MARK:- Se
+extension MainViewController {
+    func filteredMovies (for searchtext: String ) {
+        filteredMovs = movies.filter { movie in
+            return movie.title.lowercased().contains(searchtext.lowercased())
+        }
+        DispatchQueue.main.async {
+            self.moviesTableView.reloadData()
+        }
+    }
+    
+    func getStrings (for genre_ids : [Int]? ) -> String {
+        guard let genre = genre_ids else {return ""}
+        var genre_stringed : String = ""
+        for val in genre {
+           genre_stringed += ( " " + String(val))
+        }
+        return genre_stringed
+    }
 }
 
